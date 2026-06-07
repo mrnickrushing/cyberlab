1c6808376308f4a7694c4728837470ce5c6f9ef2
Nitehawk <mrnickrushing@gmail.com>
2026-06-07 12:33:30 -0700

    feat(ios): add Phase 1 iOS app - ContentView.swift

---
+++ ios/CyberLabMobile/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var authManager: AuthManager
+
+    var body: some View {
+        Group {
+            if authManager.isAuthenticated {
+                if !authManager.legalAcknowledged {
+                    LegalWarningView()
+                } else {
+                    MainTabView()
+                }
+            } else {
+                LoginView()
+            }
+        }
+        .animation(.easeInOut(duration: 0.3), value: authManager.isAuthenticated)
+    }
+}
